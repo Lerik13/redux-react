616bd8541b6406792ab0331741baa3660c2df916
Valeria <lerik13@gmail.com>
2021-12-09 13:24:24 -0700

    created reducer, Store for saving cash in bank, actions: add, remove cash

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-//import './App.css';
+import './App.css';
+
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+	const dispatch = useDispatch()
+	const cash = useSelector(state => state.cash)
+
+	const addCash = (cash) => {
+		dispatch({type: "ADD_CASH", payload: cash})
+	}
+	const getCash = (cash) => {
+		dispatch({type: "GET_CASH", payload: cash})
+	}
+
 	return (
-		<div className="App">
+		<div className={'app'}>
+			<div style={{fontSize: "3rem"}}>
+				{cash}
+			</div>
+			<div style={{display: "flex"}}>
+				<button onClick={() => addCash(Number(prompt()))}>Top up the account</button>
+				<button onClick={() => getCash(Number(prompt()))}>Withdraw from the account</button>
+			</div>
 		</div>
 	);
 }
